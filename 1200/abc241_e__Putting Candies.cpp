0d82a284f9cbc33c3da0ca33ce03567e106dc13b
Nguyen <thanhnguyen.071206@gmail.com>
2026-08-29 22:32:18 +0700

    .

--- 1200/abc241_e__Putting Candies.cpp
+++ 1200/abc241_e__Putting Candies.cpp
@@ -1,6 +1,71 @@
 #include <bits/stdc++.h>
+#define MAXN 200030
+
 using namespace std;
+using ll = long long;
+
+void sol()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    // first[j] = bước đầu tiên mà ta đứng ở vị trí j
+    vector<ll> first(n, -1);
+
+    // prefix[i] = tổng sau i bước
+    vector<ll> prefix;
+
+    prefix.push_back(0);
+
+    ll j = 0;
+    ll step = 0;
+
+    while(first[j] == -1)
+    {
+        first[j] = step;
+
+        prefix.push_back(prefix.back() + a[j]);
+
+        j = (j + a[j]) % n;
+        step++;
+    }
+
+    // Vị trí j đã xuất hiện trước đó
+    ll cycleStart = first[j];
+    ll cycleEnd = step;
+
+    ll cycleLen = cycleEnd - cycleStart;
+    ll cycleSum = prefix[cycleEnd] - prefix[cycleStart];
+
+    // Nếu k nằm hoàn toàn trước chu kỳ
+    if(k <= cycleStart)
+    {
+        cout << prefix[k];
+        return;
+    }
+
+    ll ans = prefix[cycleStart];
+
+    k -= cycleStart;
+
+    // Số chu kỳ đầy đủ
+    ans += (k / cycleLen) * cycleSum;
+
+    // Phần dư
+    ans += prefix[cycleStart + (k % cycleLen)]
+           - prefix[cycleStart];
+
+    cout << ans;
+}
+
 int main()
 {
-    cout << 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    sol();
 }
