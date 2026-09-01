120ba9ac28b91084b510920d530145e77dde9571
Nguyen <thanhnguyen.071206@gmail.com>
2026-09-02 00:06:50 +0700

    .

---
+++ AtCoder/1600/abc132_e__Hopscotch Addict.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define MAXN 100001
+#define oo 1000000000
+#define plli pair<long long, int>
+#define pii pair<int, int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+int n, m;
+void sol()
+{
+    cin >> n >> m;
+    vector<vector<int>> g;
+    int u, v;
+    for(int i = 0; i < m; i++)
+    {
+        cin >> u >> v;
+
+    }
+    int S, T; cin >> S >> T;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    sol();
+}
